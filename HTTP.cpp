1e1d313a74651abe6429fd7fd56b899c7e99af66
Chris Continanza <christopher.continanza@gmail.com>
2013-02-28 17:30:50 -0800

    new get requests

--- HTTP.cpp
+++ HTTP.cpp
@@ -1,13 +1,22 @@
 #include "HTTP.h"
 
-HTTP::HTTP(const char* url){
-  base_url = url;
+HTTP::HTTP(const char* _host){
+  host = _host;
 }
 
 int HTTP::begin(byte mac[]){
   return Ethernet.begin(mac);
 }
 
+/*
+int HTTP::post(const char* path, String body){
+  String response = "";
+  return this->post(path, body, {}, 0, &response);
+}
+
+int HTTP::post(const char* path, String body, String* response){
+  return this->post(path, body, {}, 0, response);
+}
 
 int HTTP::post(const char* path, char** headers, int num_headers,
                 String body, String* response){
@@ -15,7 +24,7 @@ int HTTP::post(const char* path, char** headers, int num_headers,
   Serial.println("connect HTTP POST");
   Serial.println(path);
 
-  if(http_client.connect(base_url, 80)){
+  if(http_client.connect(host, 80)){
     Serial.println("connected");
     // Make a HTTP request:
     String request_line = String("POST ") + String(path) + String(" HTTP/1.1");
@@ -53,13 +62,93 @@ int HTTP::post(const char* path, char** headers, int num_headers,
 
 }
 
+int HTTP::get(const char* path, char** headers, int num_headers, String* response){
+  return this->request(String("GET"), path, headers, num_headers, response);
+}
+*/
+
+int HTTP::get(const char* path){
+  return get(String(path));
+}
+
+int HTTP::get(String path){
+  String response = "";
+  char* headers[] = {};
+  String body = NULL;
+
+  return request(String("GET"), path, headers, 0, body, &response);
+}
+
+int HTTP::get(const char* path, String* response){
+  return get(String(path), response);
+}
+
+int HTTP::get(String path, String* response){
+  char* headers[] = {};
+  String body = NULL;
+
+  return request(String("GET"), path, headers, 0, body, response);
+}
+
+
+int HTTP::request(String method, String path, char** headers, int num_headers, String body, String* response){
+
+  Serial.print("Connect HTTP");
+  Serial.println();
+
+  if(http_client.connect(host, 80)){
+    Serial.println("connected");
+    // Make a HTTP request:
+    String request_line = String(method) + String(" ") + path + String(" HTTP/1.1");
+    http_client.println(request_line);
+    //Serial.println(request_line);
+    for(int i=0; i<num_headers; i++){
+      http_client.println(headers[i]);
+   //   Serial.println(headers[i]);
+    }
+
+    http_client.println("Host: " + String(host));
+
+    if(body != NULL){
+      http_client.println(String("Content-Length: ") + body.length());
+      Serial.println(String("Content-Length: ") + body.length());
+      http_client.println("Content-Type: application/x-www-form-urlencoded");
+      http_client.println("Connection: close");
+    }
+
+    http_client.println();
+
+    if(body != NULL){
+      http_client.print(body);
+      http_client.println();
+    }
+    //make sure you write all those bytes.
+    http_client.flush();
+    //aaaaaand give it some time
+    delay(10);
+
+    Serial.println("read HTTP");
+    response->concat(this->readResponse());
+
+    //cleanup
+    http_client.stop();
+
+    return 0;
+  }else{
+    Serial.println("HTTP Connection failed");
+    return 1;
+  }
+}
+
+
+/*
 int HTTP::get(const char* path, char** headers,
                 int num_headers, String* response){
 
   Serial.println("connect HTTP GET");
   Serial.println(path);
 
-  if(http_client.connect(base_url, 80)){
+  if(http_client.connect(host, 80)){
     Serial.println("connected");
     // Make a HTTP request:
     String request_line = String("GET ") + String(path) + String(" HTTP/1.1");
@@ -89,6 +178,7 @@ int HTTP::get(const char* path, char** headers,
   }
 
 }
+*/
 
 String HTTP::readResponse() {
 
--- HTTP.h
+++ HTTP.h
@@ -5,17 +5,28 @@
 class HTTP {
 
   public:
-    HTTP(const char* base_url);
+    HTTP(const char* host);
 
     int begin(byte*);
-    int get(const char*, char**, int, String* );
-    int post(const char* path, char** headers, int, String, String* response);
+
+    int request(String method, String path, char** headers, int num_headers, String body, String* response);
+
+    int get(const char*);
+    int get(String);
+
+    int get(const char*, String*);
+    int get(String, String*);
+
     /*
+    int get(const char*, char**, int, String* );
+    int post(const char* path, String body);
+    int post(const char* path, String body, String* response);
+    int post(const char* path, char** headers, int, String, String* response);
     char put(char resource);
     char delete(char resource);
     */
   private:
     EthernetClient http_client;
     String readResponse();
-    const char* base_url;
+    const char* host;
 };
